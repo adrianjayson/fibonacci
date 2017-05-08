0b05f43ae129fa8616c8adc80e4f99f7220b0b63
AJ Catambay <adrianjayson13@gmail.com>
2017-05-08 11:23:37 +0800

    fixes indention

--- fibonacci.js
+++ fibonacci.js
@@ -4,18 +4,18 @@
 */
 
 function fibonacci ( sequenceLength ) {
-	var current = 0;
+  var current = 0;
   var next = 1;
   var store = 0;
   var fibo = [];
   
   for(var i = 0; i < sequenceLength; i++) { // loops until reached the specified length of sequence
   
-  	if (i === 0) { // push 0 as the first and current number in the squence
-    	fibo.push(current);
+    if (i === 0) { // push 0 as the first and current number in the squence
+      fibo.push(current);
     } else {
-    	store = current; // Holds to value of current to get the next value, later.
-    	current = next; // moves the next number as the current number for the next iteration.
+      store = current; // Holds to value of current to get the next value, later.
+      current = next; // moves the next number as the current number for the next iteration.
       next = store + current;
       fibo.push(current);
     }
